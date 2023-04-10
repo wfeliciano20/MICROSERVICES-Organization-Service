1cfca5130d364913767d303835a2cfa24e99cfc2
William A Feliciano Rios <31602072+wfeliciano20@users.noreply.github.com>
2023-04-09 22:17:52 -0400

    Initial Configuration

--- src/test/java/com/williamfeliciano/organizationservice/OrganizationServiceApplicationTests.java
+++ src/test/java/com/williamfeliciano/organizationservice/OrganizationServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrganizationServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
